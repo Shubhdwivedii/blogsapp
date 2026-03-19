f746451de6c7b651fa21c4068f0b6be8d5acb8f7
Shubhdwivedii <shubhdwivedi101@gmail.com>
2026-03-19 21:32:42 +0530

    small fix

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,7 +7,7 @@ const Pagination = () => {
 
   return (
     <div className='fixed bottom-0 bg-white flex justify-center py-2 px-3 w-full border-t-2 border-gray-300 shadow-[0_-4px_8px_rgba(0,0,0,0.1)] items-center'>
-        <footer className='flex justify-between w-[550px] items-center'>
+        <footer className='flex justify-between w-[580px] items-center'>
             <div className='flex gap-3'>
                 { page>1 &&
                   <button onClick={() => setPage(prev => prev - 1)} className='font-normal text-sm border-2 border-gray-300 px-2 rounded-md'>Previous</button>
