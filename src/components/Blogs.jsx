b2e77112fc0b780fd8eb3bde3832b7a5165577bc
Shubhdwivedii <shubhdwivedi101@gmail.com>
2026-03-19 21:24:00 +0530

    Project Completed

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -7,24 +7,26 @@ const Blogs = () => {
     const { loading, blogs} = useContext(AppContext);
 
   return (
-    <div className='w-[550px] mt-10 mb-10'>
-        {
+    <div className='w-[630px] pt-24 pb-24 py-10'>        
+    {
             loading ? 
             (<Spinner/>) : 
             ( 
-            <div>
+            <div className='flex flex-col gap-y-6 max-w-3xl mx-auto w-11/12'>
                 {
                 blogs.map(blog => 
                     <div key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>By <span>{blog.author}</span> on <span>{blog.category}</span></p>
-                        <p>Posted on <span>{blog.date}</span></p>
-                        <p>{blog.content}</p>
+                        <h2 className='font-bold text-lg cursor-pointer hover:underline1' >{blog.title}</h2>
+                        <p className='text-sm my-1'>By <span className='italic'>{blog.author}</span> on <span className='font-semibold underline cursor-pointer'>{blog.category}</span></p>
+                        <p className='text-sm mt-[4px]'>Posted on <span>{blog.date}</span></p>
+                        <p className='text-md mt-[12px]'>{blog.content}</p>
+                        <div className='flex gap-x-2'>
                         {
                             blog.tags.map((tag, index) => 
-                                    <span key={index}>{`#${tag}`}</span>
+                                    <span key={index} className='text-blue-700 font-semibold text-xs underline cursor-pointer mt-[8px]'>{`#${tag}`}</span>
                             )
                         }
+                        </div>
                     </div>
                 )
                 }
